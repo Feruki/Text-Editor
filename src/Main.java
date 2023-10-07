8bbd814aec07909139942d321062a706fdfa5b81
yaame001 <yannick.amerkamp001@stud.fh-dortmund.de>
2023-10-07 15:33:36 +0200

    test push

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ import javafx.application.Application;
 public class Main {
     public static void main(String[] args) {
         Application.launch(Editor.class, args);
+        System.out.println("Test");
     }
 }
